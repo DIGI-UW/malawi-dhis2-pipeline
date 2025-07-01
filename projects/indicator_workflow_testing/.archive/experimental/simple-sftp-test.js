3d6b1697d722d0849eda9a50ee285ebe7b145f7f
Piotr Mankowski <pmanko@uw.edu>
2025-07-01 00:48:35 +0000

    Cleanup

--- projects/indicator_workflow_testing/.archive/experimental/simple-sftp-test.js
+++
@@ -1,9 +0,0 @@
-// Simple SFTP connection test
-// Just try to connect and list files in the root directory
-
-// OpenFN functions are available directly, no imports needed
-list('/', (state) => {
-  console.log('SFTP connection successful!');
-  console.log('Files found:', state.data);
-  return state;
-}); 
